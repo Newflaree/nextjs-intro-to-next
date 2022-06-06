23818ffde5b53b960e8c07f0493109e1fb4c34d5
Camilo López <camiloj.lopez79@gmail.com>
2022-06-05 21:34:40 -0400

    Componentes personalizados

---
+++ components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <nav>
+      <a href='/'>Hola</a>
+      <a href='/about'>About</a>
+      <a href='/contact'>Contact</a>
+    </nav>
+  );
+}
